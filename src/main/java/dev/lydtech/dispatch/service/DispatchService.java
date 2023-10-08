0ae02a5a83b2c0d05654043b26350e6c6dc7577a
galimsarov <e.galimsarov@gmail.com>
2023-10-08 19:31:02 +0500

    The Consumer

---
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -0,0 +1,10 @@
+package dev.lydtech.dispatch.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DispatchService {
+    public void process(String payload) {
+        // no-op
+    }
+}
---
+++ src/test/java/dev/lydtech/dispatch/handler/OrderCreatedHandlerTest.java
@@ -0,0 +1,24 @@
+package dev.lydtech.dispatch.handler;
+
+import dev.lydtech.dispatch.service.DispatchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class OrderCreatedHandlerTest {
+    private OrderCreatedHandler handler;
+    private DispatchService dispatchServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        dispatchServiceMock = mock(DispatchService.class);
+        handler = new OrderCreatedHandler(dispatchServiceMock);
+    }
+
+    @Test
+    void listen() {
+        handler.listen("payload");
+        verify(dispatchServiceMock, times(1)).process("payload");
+    }
+}
